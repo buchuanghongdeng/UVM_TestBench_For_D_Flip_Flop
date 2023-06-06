b33bc779cba0ffaab1423bd0b02e12d8dff57b7a
buchuanghongdeng <129075110+buchuanghongdeng@users.noreply.github.com>
2023-06-06 17:53:46 +0800

    Update design.sv
    practice

--- design.sv
+++ design.sv
@@ -2,7 +2,7 @@ module dff(d,clk,rst,q);
     input d;
     input clk;
     input rst;
-    output reg q;
+    output reg q;//forget reg
 
     always @(posedge clk or posedge rst) begin
         if(rst==1) begin 
